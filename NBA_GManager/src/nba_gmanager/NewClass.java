ffb8a807d829d3eae41b4fe379308cd1bbe8d6d8
Yah Rou Jun <rjy0713@gmail.com>
2024-05-10 02:44:40 +0800

    testing

---
+++ NBA_GManager/src/nba_gmanager/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Yah Rou Jun
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("try");
+    }
+    
+}
